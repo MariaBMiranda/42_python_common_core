9fa3932de86450ec3570642e1e486e818342fdb1
mabarret <mirandabmaria@gmail.com>
2026-08-25 19:40:16 -0300

    exam6update

---
+++ Exam Rank 6/Level 0/py_union_find_system.py
@@ -0,0 +1,26 @@
+class UnionFind:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x: int) -> int:
+        root = x
+        while self.parent[root] != root:
+            root = self.parent[root]
+        while self.parent[x] != root:  # compressão de caminho
+            self.parent[x], x = root, self.parent[x]
+        return root
+
+    def union(self, x: int, y: int) -> bool:
+        rx, ry = self.find(x), self.find(y)
+        if rx == ry:
+            return False
+        if self.rank[rx] < self.rank[ry]:
+            rx, ry = ry, rx
+        self.parent[ry] = rx
+        if self.rank[rx] == self.rank[ry]:
+            self.rank[rx] += 1
+        return True
+
+    def connected(self, x: int, y: int) -> bool:
+        return self.find(x) == self.find(y)
